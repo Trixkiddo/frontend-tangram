37efe639a530c51aa0a66718c149f560150392c3
Laurent <magrez0laurent@gmail.com>
2022-03-24 08:00:20 +0100

    comment & button & effects

--- js/changeShape.js
+++ js/changeShape.js
@@ -1,9 +1,10 @@
 const shapeArray = ["pomme", "sapin", "swan", "tortue"];
 
-const shapeNodeList = document.querySelectorAll(".shape");
 const square = document.querySelector(".container-general");
+const shapeList = document.querySelectorAll(".js-get-shape")
 
-for (shape of shapeNodeList) {
+// add event listener
+for (shape of shapeList) {
   shape.addEventListener("click", changeShape);
 }
 
@@ -11,20 +12,21 @@ let lastestAddedClass = "";
 
 function changeShape(e) {
   let whichShape = e.target;
-  let shapeLiIndex = Array.prototype.indexOf.call(shapeNodeList, whichShape);
+  let shapeLiIndex = Array.prototype.indexOf.call(shapeList, whichShape);
 
+  // if clicked button isnt the same as the lastest class
   if (shapeArray[shapeLiIndex] !== lastestAddedClass) {
-
+    //clean classList from preview shape class
     if (square.classList.contains(lastestAddedClass)) {
       square.classList.remove(lastestAddedClass);
     }
-
+    // add corresponding ( clicked button ) shape class to the Classlist
     square.classList.add(shapeArray[shapeLiIndex]);
     lastestAddedClass = shapeArray[shapeLiIndex];
   } else {
+    // remove class if same button clicked once again for toggle effect
     square.classList.remove(lastestAddedClass);
+    //need to reset lastestclass for initial check
     lastestAddedClass = "";
   }
 }
-
-
